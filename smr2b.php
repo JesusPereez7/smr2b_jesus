c336c5bcdabb9fb657b363fc5c07ccab62071429
unknown <jesuspereez1809@gmail.com>
2019-10-01 17:48:50 +0200

    Jesus

--- smr2b.php
+++ smr2b.php
@@ -2,10 +2,26 @@
 Jesus
 <a href="http://192.168.0.115/smr2b_adrian/smr2b.php">Adrian</a>
 
+
+<form>
+<input name = "email" type = "email">
+<button> email </button>
+</form>
+
+
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
 fclose($f)
+?>
+
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
