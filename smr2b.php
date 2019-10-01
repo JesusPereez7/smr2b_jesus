8af72c8b8da35cc86d818b398bfa70bf85e0c59e
unknown <jesuspereez1809@gmail.com>
2019-10-01 16:26:21 +0200

    Jesus Perez

---
+++ smr2b.php
@@ -0,0 +1,10 @@
+<a href="http://192.168.0.101/smr2b_ivan/smr2b.php">Antetokounmpo</a>
+Jesus
+<a href="http://192.168.0.115/smr2b_adrian/smr2b.php">Adrian</a>
+
+<?php
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
+fclose($f)
+?>
