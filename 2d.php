286685f2f0238730dce1b21e82ae6b9bcac02ec5
unknown <jesuspereez1809@gmail.com>
2019-10-11 18:20:07 +0200

    Jesus

--- 2d.php
+++ 2d.php
@@ -1,4 +1,22 @@
 <?php
-
-for($i=1;$i<=10;$i=$i+1)
-	echo '1x'.$i.'='.(1*$i).'<br>';
+function meses($n){
+		$meses=[
+	'enero',
+	'febrero',
+	'marzo',
+	'abril',
+	'mayo',
+	'junio',
+	'julio',
+	'agosto',
+	'septiembre',
+	'octubre',
+	'noviembre',
+	'diciembre',
+	];
+	return $meses[$n-1];
+}
+for($i=1;$i<=12;$i=$i+1){
+if(!strstr (meses($i),'r'))
+	echo meses($i).'<br>';
+}
