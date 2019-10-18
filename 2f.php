bd92869b34d6c39d029a8cb01d609e4a0dbf2a09
unknown <jesuspereez1809@gmail.com>
2019-10-18 17:01:02 +0200

    Jesus

--- 2f.php
+++ 2f.php
@@ -1,7 +1,19 @@
 <?php
-for($o=2;$o<=10;$o=$o+2){
-	for($i=1;$i<=10;$i=$i+1){
-		echo $o.'x'.$i.'='.($o*$i).'<br>';
-	}
-	echo'<br>';
+function meses($n){
+		$meses=[
+	'enero',
+	'febrero',
+	'marzo',
+	'abril',
+	'mayo',
+	'junio',
+	'julio',
+	'agosto',
+	'septiembre',
+	'octubre',
+	'noviembre',
+	'diciembre',
+	];
+	return $meses[$n-1];
 }
+	echo meses(1).'<br>';
